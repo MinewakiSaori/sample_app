91f15320d1b1ded29057c89b588f83b8c4991736
Minewaki <s.m-0522@hotmail.co.jp>
2017-04-20 04:49:26 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   validates :name,  presence: true, length: { maximum: 50 }
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
 end
